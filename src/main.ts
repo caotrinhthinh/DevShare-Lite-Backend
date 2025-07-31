99557a21472510e7fa13203dd757266cf540d882
caotrinhthinh <caotrinhthinh@gmail.com>
2025-07-31 22:33:13 +0700

    Fix CORS

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:8080',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   });
 
